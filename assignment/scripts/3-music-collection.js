53e03b85530e5773cb92312365ede149a93dacc3
Kenny Ferguson <121765690+Kennyfergy@users.noreply.github.com>
2023-08-16 17:32:56 -0500

    added new function: addToCollection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,21 @@
-console.log('***** Music Collection *****')
+console.log("***** Music Collection *****");
+
+// Create an empty array called 'collection'
+const collection = [];
+
+// Add a function named 'addToCollection'
+function addToCollection(title, artist, yearPublished) {
+  // Create a new album object
+  const album = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished,
+  }; // end create new object
+
+  // Add the album object to the end of the 'collection' array
+  collection.push(album);
+
+  // Return the newly created album object
+  return album;
+} // end addToCollection
+console.log("The collection should be empty test:", collection);
